bc592ee5fd5bb9c06ab5399ab5ada69e10149a3c
TiltedAgency <info@tiltedagency.com>
2025-08-19 11:27:33 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'   // ← keep the .tsx at the end
+import App from './App.tsx'   // ← keep the .tsx and the capital A
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
